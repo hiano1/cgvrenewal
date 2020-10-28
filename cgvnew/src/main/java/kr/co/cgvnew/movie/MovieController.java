4095c036821c187af055981e42bd643f7efcd156
username <user@email.mail>
2020-10-28 14:34:18 +0900

    j

--- cgvnew/src/main/java/kr/co/cgvnew/movie/MovieController.java
+++ cgvnew/src/main/java/kr/co/cgvnew/movie/MovieController.java
@@ -1,12 +1,7 @@
 package kr.co.cgvnew.movie;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MovieController {
 
-	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
 	@Autowired
 	private MService service;
 
--- cgvnew/src/main/java/kr/co/cgvnew/movie/MovieRestController.java
+++ cgvnew/src/main/java/kr/co/cgvnew/movie/MovieRestController.java
@@ -4,8 +4,7 @@ package kr.co.cgvnew.movie;
 import java.util.List;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RequestMapping;
