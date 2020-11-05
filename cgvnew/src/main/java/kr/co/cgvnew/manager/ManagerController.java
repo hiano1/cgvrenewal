b72fc7da11ac5655e33ba5c16b4087da25c0af91
user <user@ICT3-24>
2020-11-05 15:10:46 +0900

    수정

--- cgvnew/src/main/java/kr/co/cgvnew/manager/ManagerController.java
+++ cgvnew/src/main/java/kr/co/cgvnew/manager/ManagerController.java
@@ -1,5 +1,17 @@
 package kr.co.cgvnew.manager;
 
-public class ManagerController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ManagerController {
+	@Autowired
+	private ManagerService service;
+	
+	public String timeList() {
+		List<ManagerDTO> list = service.timelist();
+		return "/manager/movieSet";
+	}
 }
---
+++ cgvnew/src/main/java/kr/co/cgvnew/manager/ManagerDAO.java
@@ -0,0 +1,9 @@
+package kr.co.cgvnew.manager;
+
+import java.util.List;
+
+public interface ManagerDAO {
+
+	List<ManagerDTO> timeList();
+
+}
