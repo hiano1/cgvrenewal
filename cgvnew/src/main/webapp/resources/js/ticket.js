82a27e47b881c3718c2d4c417875253b3e511c5d
user <user@ICT3-24>
2020-11-06 10:27:59 +0900

    sql 폴더 위치이동

--- cgvnew/src/main/webapp/resources/js/ticket.js
+++ cgvnew/src/main/webapp/resources/js/ticket.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 			}); //each 
 			}
 			, "json");
-		}); //click
+		}); //click lcode
 	$(document).on("click","a[id^=mcode]",function(){
 		$("#selM").text($(this).text());
 	});
@@ -42,38 +42,42 @@ $(document).ready(function() {
 	//next 버튼 눌릴때 시간표페이지로 넘어감
 	//side var text 가 null이면 false
 	$("#nextBtn").click(function(){
-		
-										
-	});	
-	let mcode;
-	switch ($(this).text()) {
-		case 'car cinema': mcode = 1; break;
-		case 'cgv강남': mcode = 2; break;
-		case 'cgv강변': mcode = 3; break;
-		case 'cgv홍대': mcode = 4; break;
-		case 'cgv경기광주': mcode = 5; break;
-		case 'cgv고양행신': mcode = 6; break;
-		case 'cgv계양': mcode = 7; break;
-		case 'cgv남주안': mcode = 8; break;
-		case 'cgv강릉': mcode = 9; break;
-		case 'cgv원주': mcode = 10; break;
-		case 'cgv당진': mcode = 11; break;
-		case 'cgv대전': mcode = 12; break;
-		case 'cgv대구': mcode = 13; break;
-		case 'cgv대구수성': mcode = 14; break;
-		case 'cgv남포': mcode = 15; break;
-		case 'cgv대연': mcode = 16; break;
-		case 'cgv거제': mcode = 17; break;
-		case 'cgv고성': mcode = 18; break;
-		case 'cgv광양': mcode = 19; break;
-		case 'cgv광양 엘에프스퀘어': mcode = 20; break;
-		}; //switch
+		let mcode;
+		switch ($("#selM").text()) {
+			case 'car cinema': mcode = 1; break;
+			case 'cgv강남': mcode = 2; break;
+			case 'cgv강변': mcode = 3; break;
+			case 'cgv홍대': mcode = 4; break;
+			case 'cgv경기광주': mcode = 5; break;
+			case 'cgv고양행신': mcode = 6; break;
+			case 'cgv계양': mcode = 7; break;
+			case 'cgv남주안': mcode = 8; break;
+			case 'cgv강릉': mcode = 9; break;
+			case 'cgv원주': mcode = 10; break;
+			case 'cgv당진': mcode = 11; break;
+			case 'cgv대전': mcode = 12; break;
+			case 'cgv대구': mcode = 13; break;
+			case 'cgv대구수성': mcode = 14; break;
+			case 'cgv남포': mcode = 15; break;
+			case 'cgv대연': mcode = 16; break;
+			case 'cgv거제': mcode = 17; break;
+			case 'cgv고성': mcode = 18; break;
+			case 'cgv광양': mcode = 19; break;
+			case 'cgv광양 엘에프스퀘어': mcode = 20; break;
+			}; //switch
 	
-	$.ajax({
-	url:"/cgvnew/ticketrest/seat"
-	,data:{mcode : mcode}
-	,method:"GET"
-	,dataType:"json"
+		$.ajax({
+		url:"/cgvnew/ticketrest/seat"
+		,type:"GET"
+		,dataType:"json"
+		,contentType: "application/json"
+		,data:{mcode : mcode}
+		,success : function(result){
+	 			$(".ticketcon").empty();
+	 			$(".ticketcon").append
+	 			
+			}//success								
+		});//ajax
 	
-	});								
+	});	//click next							
 });//ready
